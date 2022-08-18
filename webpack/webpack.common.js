aba97672c911421b387123b63ad06043f9f09db1
ajith0110 <santhan.ajith50@gmail.com>
2022-08-18 13:44:13 +0530

    test commit

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,39 +1,44 @@
-const path =require('path');
-const HtmlWebpackPlugin =require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports={
-    entry:path.resolve(__dirname,'..','./src/index.tsx'),
-    resolve:{
-        extensions:['.tsx','.ts','.js']
-    },
-
-    module:{
-        rules:[{
-            test:/\.(ts|js)x?$/,
-            exclude:/node_modules/,
-            use:[
-                {
-                    loader:'babel-loader'
-                }
-            ]
-        },{
-            test:/\.(sass|css|scss)$/,
-            use:['style-loader','css-loader']
-        }, {
-            test: /\.(png|svg|jpg|jpeg|gif)/,
-            type: 'asset/resource'
-          },{
-            test: /\.(png|svg|jpg|jpeg|gif)/,
-            type: 'asset/inline'
-          }]
-    },
-    output:{
-    path:path.resolve(__dirname,'..','./build'),
-    filename:'bundle.js'
-    },
-  
-    plugins:[new HtmlWebpackPlugin({
-        template:path.resolve(__dirname,'..','./src/index.html')
-    })]
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(sass|css|scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,13 +1,11 @@
-const ReactRefeshWebpackPlugin =require('@pmmmwh/react-refresh-webpack-plugin')
+const ReactRefeshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-module.exports={
-    mode:'development',
-    devtool:'cheap-module-source-map',
-    devServer:{
-        hot:true,
-        open:true
-    },
-    plugins:[
-    new ReactRefeshWebpackPlugin()
-    ]
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true,
+  },
+  plugins: [new ReactRefeshWebpackPlugin()],
 }
